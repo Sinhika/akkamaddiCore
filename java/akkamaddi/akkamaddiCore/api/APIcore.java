67db97f4c865f97fdcac9b249ccf36bf43e6c2ca
Sinhika <sinhika@republicofnewhome.org>
2014-10-08 21:24:00 -0500

    1.10 update
    Added AkkaWorldGenHelper class, which is just like Simple Ores2's
    WorldGenHelper class, except that it checks against
    Block.isOreGenReplaceable.

--- java/akkamaddi/akkamaddiCore/api/APIcore.java
+++ java/akkamaddi/akkamaddiCore/api/APIcore.java
@@ -12,7 +12,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "akkamaddicore", name = "akkamaddi's Core API", version = "1.7.10-1.0.1", 
+@Mod(modid = "akkamaddicore", name = "akkamaddi's Core API", version = "1.7.10-1.1.0", 
 	 dependencies = "required-after:simpleores; after:MoCreatures")
 
 public class APIcore 
