6bb0bc975a61d1cdc893cf77465ae066d9b715a9
Sinhika <sinhika@republicofnewhome.org>
2014-10-15 19:28:28 -0500

    v1.0.2, NOT 1.1.0!

--- java/akkamaddi/akkamaddiCore/api/APIcore.java
+++ java/akkamaddi/akkamaddiCore/api/APIcore.java
@@ -12,7 +12,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "akkamaddicore", name = "akkamaddi's Core API", version = "1.7.10-1.1.0", 
+@Mod(modid = "akkamaddicore", name = "akkamaddi's Core API", version = "1.7.10-1.0.2", 
 	 dependencies = "required-after:simpleores; after:MoCreatures")
 
 public class APIcore 
